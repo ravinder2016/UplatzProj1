9b24a80cc9ea1d4e122bf449fe1c12040652621b
ravinder2016 <ravinder.nitw@gmail.com>
2021-06-05 16:02:55 +0530

    Initial Commit

---
+++ src/main/java/in/uplatz/com/UplatzProj1Application.java
@@ -0,0 +1,13 @@
+package in.uplatz.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UplatzProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UplatzProj1Application.class, args);
+	}
+
+}
